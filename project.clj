6c307526e23226a4a07edb0e0dc3599ddc4e8f33
dat <dat@coachingcloud.com>
2017-01-20 02:13:29 +0700

    implement process file with unzip and json parse

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Openfda project."
   :dependencies [
     [org.clojure/clojure "1.8.0"]
+    [org.clojure/data.json "0.2.6"]
     ;; First you'll need to add Korma as a dependency in your lein/cake project:
     [korma "0.4.3"]
     ;; You'll also need the JDBC driver for your database. These are easy to find if
@@ -9,5 +10,6 @@
     [org.clojure/java.jdbc "0.6.1"]
     [mysql/mysql-connector-java "5.1.35"]
     ;; Example for postgres:
-    [org.postgresql/postgresql "9.2-1002-jdbc4"]]
+    [org.postgresql/postgresql "9.2-1002-jdbc4"]
+    [me.raynes/fs "1.4.6"]]
   :main openfda)
--- src/openfda.clj
+++ src/openfda.clj
@@ -1,22 +1,35 @@
 (ns openfda
-  (:require [korma.core :as korma]
+  (:require [clojure.java.io :as io]
+            [clojure.data.json :as json]
+            [korma.core :as korma]
             [korma.db :as db]
+            [me.raynes.fs :as fs]
+            [me.raynes.fs.compression :as compression]
             [clojure.string :as str])
   (:gen-class))
 
 (def db-config
   (db/mysql {:db "openfda"
-                :user "homestead"
-                :password "secret"
-                ;; optional keys
-                :host "192.168.10.10"
-                :port "3306"
-                :delimiters ""}))
+             :user "homestead"
+             :password "secret"
+             ;; optional keys
+             :host "192.168.10.10"
+             :port "3306"
+             :delimiters ""}))
+
+(def file "/home/rook/Projects/Code/clojure/data/openfda.adr/8baea810-d888-11e6-9192-010a891943b3")
+(defn process [filename]
+  (-> filename
+      io/input-stream
+      java.util.zip.GZIPInputStream.
+      slurp
+      json/read-str))
+
 
 (db/defdb pg db-config)
 
 (korma/defentity report)
 
 (defn -main []
-  (println "This is the main function")
+  (print (process file))
   (print (korma/select report)))
